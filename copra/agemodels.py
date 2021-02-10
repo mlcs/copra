85093d30943400c91532b81d0b5e6e9a60a9a111
Bedartha Goswami <bedartha.goswami@uni-tuebingen.de>
2021-02-10 21:23:49 +0100

    add __all__ to agemodels and proxyrecords modules

--- copra/agemodels.py
+++ copra/agemodels.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 
+__all_ = [
+        'agemods'
+        ]
+
 def agemods():
     """Dummy function for fist git commit"""
     print("Hello World! This is Age Mods here!")
--- copra/proxyrecords.py
+++ copra/proxyrecords.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 
+__all__ = [
+        'prxmods'
+        ]
+
 def prxmods():
     """Dummy function for fist git commit"""
     print("Hello World! This is Proxy Mods here!")
